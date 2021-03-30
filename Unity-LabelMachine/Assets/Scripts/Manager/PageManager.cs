a2efd3ea5bdea7d38055a1705bd20fe3f603dcdd
Eraslank <kayraeraslan98@gmail.com>
2021-03-30 15:59:58 +0300

    Created Page Manager & Generic Singleton Class Controller

---
+++ Unity-LabelMachine/Assets/Scripts/Manager/PageManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Linq;
+using System.Threading;
+using UnityEngine.UI;
+
+public class PageManager : MonoBehaviourSingleton<PageManager>
+{
+    public List<Page> pages;
+    Page currentPage;
+    public GameObject loadingPage;
+
+    // Use this for initialization
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        SetPage(GetPage(EPageName.Launch), false);
+        SetPage(GetPage(EPageName.None), true);
+    }
+
+    public void OpenUrl(string url)
+    {
+        Application.OpenURL(url);
+    }
+
+    public void ChangePage(Page page)
+    {
+        if (currentPage != null && currentPage == page)
+            return;
+        if (currentPage != null)
+            SetPage(currentPage, false);
+        currentPage = page;
+        SetPage(currentPage, true);
+    }
+    public void ChangePage(EPageName pageName, bool forcePageChange = false)
+    {
+        if (currentPage != null && currentPage.pageName == pageName && !forcePageChange)
+            return;
+        Page page = GetPage(pageName);
+        if (currentPage != null)
+            SetPage(currentPage, false);
+        currentPage = page;
+        SetPage(currentPage, true);
+    }
+    void SetPage(Page page, bool showPage)
+    {
+        if (showPage)
+        {
+            IConfigurablePage iCP = page.GetComponent<IConfigurablePage>();
+            if (iCP != null)
+                iCP.ConfigurePage();
+        }
+        page.isVisible(showPage);
+    }
+    public Page GetPage(EPageName page)
+    {
+        return pages.Select(p => p).Where(p => p.pageName == page).First();
+    }
+}
---
+++ Unity-LabelMachine/Assets/Scripts/Page.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour 
+{
+	public EPageName pageName;
+	public void isVisible(bool state)
+	{
+		gameObject.SetActive(state);
+	}
+}
