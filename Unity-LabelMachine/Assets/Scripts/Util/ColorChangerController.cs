1b33d088f86829742faf860fcbc2285f84d359e6
Eraslank <kayraeraslan98@gmail.com>
2021-03-30 15:38:17 +0300

    Fix GitIgnore Not Working Properly

---
+++ Unity-LabelMachine/Assets/Scripts/Util/ColorChangerController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class ColorChangerController : MonoBehaviour
+{
+    [SerializeField] private bool isForeGround = true;
+    Image img;
+    void Start()
+    {
+        img = GetComponent<Image>();
+        img.color = (isForeGround) ? LabelMachineColors.ForegroundColor : LabelMachineColors.BackgroundColor;
+    }
+}
