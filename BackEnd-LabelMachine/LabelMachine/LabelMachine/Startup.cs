9e5d5889f3d7156da3cb75cb85d62c80f25d9cd3
Eraslank <kayraeraslan98@gmail.com>
2021-04-20 15:18:33 +0300

    Connecting To DB

--- BackEnd-LabelMachine/LabelMachine/LabelMachine/Startup.cs
+++ BackEnd-LabelMachine/LabelMachine/LabelMachine/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LabelMachine.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace LabelMachine
 {
@@ -25,6 +27,12 @@ namespace LabelMachine
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<LabelMachineDatabaseSettings>(
+                Configuration.GetSection(nameof(LabelMachineDatabaseSettings)));
+
+            services.AddSingleton<ILabelMachineDatabaseSettings>(sp =>
+                sp.GetRequiredService<IOptions<LabelMachineDatabaseSettings>>().Value);
+
             services.AddSwaggerGen();
             services.AddControllers();
         }
